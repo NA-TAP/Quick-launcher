3b80d69aab3900ad73d3733044332cabc50ed30b
AnayTaparia <anay.taparia@gmail.com>
2025-06-14 14:22:48 +0530

    Update UnitConvertor.py

--- UnitConvertor.py
+++ UnitConvertor.py
@@ -1,2 +1,2 @@
-si_to_imperial_length = {"km_to_miles": 0.62137,"cm_to_inch": 0.3937,"mm_to_inch": 0.0393,"dm_to_ft": 0.3280,"m_to_yd": 1.0936,"m_to_ft": 3.28084,"m_to_inch": 39.3701,"cm_to_ft": 0.0328084,"mm_to_ft": 0.00328084,"km_to_yd": 1093.61,"km_to_ft": 3280.84,"cm_to_yd": 0.0109361,}
-si_to_imperial_volume = {"km^3_to_mile^3": 
+si_to_imperial_length = {"km to miles": 0.62137,"cm to inch": 0.3937,"mm to inch": 0.0393,"dm to ft": 0.3280,"m to yd": 1.0936,"m to ft": 3.28084,"m to inch": 39.3701,"cm_to_ft": 0.0328084,"mm_to_ft": 0.00328084,"km_to_yd": 1093.61,"km_to_ft": 3280.84,"cm_to_yd": 0.0109361,}
+si_to_imperial_volume = {"km3_to_mile3": 
